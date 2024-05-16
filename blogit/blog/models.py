a73d184007400d8f6ffd2a02d096f9f24aab3055
APIYOJENNIFER <apiyojennifer4@gmail.com>
2024-05-16 10:22:34 +0300

    Create the blog model

--- blogit/blog/models.py
+++ blogit/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+
 # Create your models here.
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by("-first_published_at")
+        context["blogpages"] = blogpages
+        return context
+
+    content_panels = Page.content_panels + [FieldPanel("intro")]
+
+
+class BlogPage(Page):
+    image = models.ForeignKey(
+        "wagtailimages.Image", null=True, blank=True, on_delete=models.SET_NULL
+    )
+    date = models.DateField("Publish date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("image"),
+        FieldPanel("date"),
+        FieldPanel("intro"),
+        FieldPanel("body"),
+    ]
