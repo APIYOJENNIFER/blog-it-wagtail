280d3df26abfe7f96d0ff5ab0a249fa8f443e3dc
APIYOJENNIFER <apiyojennifer4@gmail.com>
2024-05-20 12:30:35 +0300

    Fix lint warnings

--- blogit/home/models.py
+++ blogit/home/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+"""Home model"""
 
 from wagtail.models import Page
 from wagtail.fields import RichTextField
@@ -6,8 +6,10 @@ from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
+    """Model to define the fields and structure of the Home Page"""
+
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body'),
+        FieldPanel("body"),
     ]
