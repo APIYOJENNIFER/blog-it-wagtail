a8364c795b272e18ac341c6cd7e3b9c30657d2b1
APIYOJENNIFER <apiyojennifer4@gmail.com>
2024-05-29 15:33:18 +0300

    Remove blog_title

--- blogit/home/models.py
+++ blogit/home/models.py
@@ -12,13 +12,11 @@ class HomePage(Page):
     image = models.ForeignKey(
         "wagtailimages.Image", null=True, blank=True, on_delete=models.SET_NULL
     )
-    blog_title = models.CharField(max_length=255, blank=False, null=True)
 
     content_panels = Page.content_panels + [
         MultiFieldPanel(
             [
                 FieldPanel("image"),
-                FieldPanel("blog_title"),
             ]
         )
     ]
